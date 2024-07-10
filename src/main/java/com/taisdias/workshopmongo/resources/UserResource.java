950fb3ed4598725d665f4892f28bcb2cb18dab19
Tais <taisdias76@gmail.com>
2024-07-09 21:00:55 -0300

    Insercao de usuario com POST

--- src/main/java/com/taisdias/workshopmongo/resources/UserResource.java
+++ src/main/java/com/taisdias/workshopmongo/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.taisdias.workshopmongo.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.taisdias.workshopmongo.domain.User;
 import com.taisdias.workshopmongo.dto.UserDTO;
@@ -33,5 +37,14 @@ public class UserResource {
   public ResponseEntity<UserDTO> findById(@PathVariable String id) {
     User user = service.findById(id);
     return ResponseEntity.ok().body(new UserDTO(user));
+  }
+
+  @RequestMapping(method=RequestMethod.POST)
+  public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+    User user = service.fromDTO(userDTO);
+    user = service.insert(user);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+    return ResponseEntity.created(uri).build();
   }
 }
--- src/main/java/com/taisdias/workshopmongo/services/UserService.java
+++ src/main/java/com/taisdias/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.taisdias.workshopmongo.domain.User;
+import com.taisdias.workshopmongo.dto.UserDTO;
 import com.taisdias.workshopmongo.repository.UserRepository;
 import com.taisdias.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
   public User findById(String id) {
     Optional<User> user = repository.findById(id);
     return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+  }
+
+  public User insert(User obj) {
+    return repository.insert(obj);
+  }
+
+  public User fromDTO(UserDTO objDto) {
+    return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
   }
 }
