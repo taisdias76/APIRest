0bbffbc22313237c6718644ece983640e3c80773
Tais <taisdias76@gmail.com>
2024-07-13 22:58:45 -0300

    Consulta simples com Query

--- src/main/java/com/taisdias/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/taisdias/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.taisdias.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.taisdias.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
+  List<Post> searchTitle(String title);
+  
   List<Post> findByTitleContainingIgnoreCase(String text);
 }
